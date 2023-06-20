d26725faeab69213b4480c75c00dc05826910f86
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2023-06-20 14:23:20 +0300

    Get version from tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
+# Remove "v" from the start of the tag e.g. v1.0.0 -> 1.0.0
+LATEST_GIT_TAG="${LATEST_GIT_TAG:1}"
 # Update package.json's version field
 npm version $LATEST_GIT_TAG --no-commit-hooks --no-git-tag-version
 
